a42e7bd12bf1d878c374bd0b12005112c8e896ed
Drew <andrej.siekel@gmail.com>
2024-09-20 00:12:12 +0200

    Refactor API URL and add quote functionality

--- web/data/scripts/ping.js
+++ web/data/scripts/ping.js
@@ -1,23 +1,22 @@
-const api = 'http://localhost:3000';
-const pingUrl = api + '/ping'; 
+// Define the API URL as a constant
+const API = 'http://localhost:3000';
+//const API = 'https://api.ndrew.sk';
 
+// Fetches the status data from /ping endpoint and updates the corresponding h2 elements
 async function fetchStatus() {
     try {
-        const response = await fetch(pingUrl); // Fetch the data from the API
+        const response = await fetch(API + '/ping'); // Make the API request
         const data = await response.json(); // Parse the JSON response
-
         // Mapping of URLs to their corresponding h2 element IDs
         const statusMap = {
             "https://ndrew.sk": "ndrewSkStatus",
             "https://obedik.ndrew.sk": "obedikStatus",
             "https://quotes.ndrew.sk": "quotesStatus"
         };
-
         // Iterate over the response and update the corresponding elements
         data.forEach(item => {
             const elementId = statusMap[item.url]; // Get the corresponding element ID
             const statusElement = document.getElementById(elementId);
-
             if (statusElement) {
                 statusElement.textContent = item.status; // Simply set the text content
             }
@@ -28,4 +27,4 @@ async function fetchStatus() {
 }
 
 // Call the function when the page loads
-document.addEventListener('DOMContentLoaded', fetchStatus);
+window.onload = fetchStatus;
---
+++ web/data/scripts/quote.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const endpoint = 'https://api-quotes.ndrew.sk/randomquote';
+
+    fetch(endpoint)
+        .then(response => response.json())
+        .then(data => {
+            const quoteText = `"${data.quote}" - ${data.author}`;
+            document.getElementById('quotemarquee').textContent = quoteText;
+        })
+        .catch(error => console.error('Error fetching the quote:', error));
+});
